fab4b0da788823c8b45b93c721f4918e695ef414
Alex <alexandre.aceldy@gmail.com>
2017-04-01 15:54:33 +0200

    creation KEY

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var server = express();
 var bodyParser = require("body-parser");
+var fs = require("fs");
+var contenu;
+
+contenu = fs.readFileSync("KEY", "UTF-8");
 
 server.use(bodyParser.urlencoded({ extended: true }));
 
@@ -9,7 +13,15 @@ server.get('/', function(request, response) {
 	response.sendFile(__dirname + '/index.html');
 });
 
+server.get("http://https://translate.yandex.net/api/v1.5/tr/translate ?", function(request, response)) {
+	
+});
+
 server.post('/', function(request, response) {
-	response.send(request.body.textarea);
+	var From = request.body.from.toLowerCase();
+	var To = request.body.to.toLowerCase();
+	var Text = request.body.textarea;
+	
+	response.send(From + ' ' + To + ' ' + Text + ' ' + contenu);
 });
 server.listen(8080);
