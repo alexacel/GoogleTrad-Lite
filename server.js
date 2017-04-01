11bc27c24f133aafa5ea8361d1e87be67d22ca13
Alex <alexandre.aceldy@gmail.com>
2017-04-01 14:00:43 +0200

    recuperation et parsing requete POST de page vers serveur

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var server = express();
+var bodyParser = require("body-parser");
+
+server.use(bodyParser.urlencoded({ extended: true }));
+
+//affichage du html
 server.get('/', function(request, response) {
 	response.sendFile(__dirname + '/index.html');
 });
+
 server.post('/', function(request, response) {
-	response.send(request);
+	response.send(request.body.textarea);
 });
 server.listen(8080);
