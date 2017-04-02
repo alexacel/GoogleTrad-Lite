8508f243becaab33e116a9cff6c944f709b27701
Alex <alexandre.aceldy@gmail.com>
2017-04-02 21:53:25 +0200

    menage

--- public/display.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('Output').innerHTML="Salut";
-document.getElementById('To').value="FR";
-document.getElementById('From').value="EN";
-document.getElementById('Input').value="Hi";
