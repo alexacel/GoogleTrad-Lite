fba19fe75093305fe76512f2065a17a463bed709
Alex <alexandre.aceldy@gmail.com>
2017-04-02 21:41:27 +0200

    testu ok

---
+++ public/display.js
@@ -0,0 +1,4 @@
+document.getElementById('Output').innerHTML="Salut";
+document.getElementById('To').value="FR";
+document.getElementById('From').value="EN";
+document.getElementById('Input').value="Hi";
