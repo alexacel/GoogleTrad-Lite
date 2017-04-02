fc0bcd78c753d4b3bf8b6af137b260a5fc15bb04
Alex <alexandre.aceldy@gmail.com>
2017-04-02 19:58:15 +0200

    conservation des donnees de formulaire

--- server.js
+++ server.js
@@ -7,12 +7,17 @@ var APIrequest = require("request");
 var MongoClient = require('mongodb').MongoClient;
 var yandexURL = "https://translate.yandex.net/api/v1.5/tr.json/translate?"
 
-function writeResponse(translatedText){
-	if(typeof translatedText !== 'string'){ //FIXME apostrophes autour de string ?
-		//TODO
-	}
+function writeResponse(from, to, ori, translatedText){
+/*	if((typeof translatedText !== 'string') || (typeof from != 'string') || (typeof to != 'string') || (typeof ori != 'string')){
+		console.log("wrong parameters in \'writeResponse\'");
+		throw TypeError
+	}	*/
 	
-	fs.writeFileSync(__dirname+"/public/display.js", "document.getElementById('Output').innerHTML=\""+translatedText+"\";", "UTF-8");
+	fs.writeFileSync(__dirname+"/public/display.js",  "document.getElementById('Output').innerHTML=\""+translatedText+"\";\n"
+													+ "document.getElementById('To').value=\""+to+"\";\n"
+													+ "document.getElementById('From').value=\""+from+"\";\n"
+													+ "document.getElementById('Input').value=\""+ori+"\";\n"
+													, "UTF-8");
 	
 }
 
@@ -21,90 +26,10 @@ yandexKey = fs.readFileSync("KEY", "UTF-8");
 
 server.use(bodyParser.urlencoded({ extended: true }));
 
-//connect to database
-/*
-var DB;
-MongoClient.connect("mongodb://localhost/data", function(error, db) {
-	if (error) {
-		console.log("error during connexion to database");
-		throw error;
-	}
-	
-	DB=db;
-	console.log("connected to database");
-
-});
-*/
-
-
-
-
-/**********tests mongodb*************/
-/*
-MongoClient.connect("mongodb://localhost/test2", function(error, db) {
-    if (error) {
-		console.log("error during connexion to database");
-		throw error;
-	}
-
-    console.log("Connected to database");
-	
-	//nouvelle collection
-	var collection = db.collection("personnages");
-	
-	//insertion d'un document
-	var objNew = { name: "coucou", game: "Super Mario World", date: "1994" }; 	
-
-	collection.insert(objNew, null, function (error, results) {
-		if (error) {
-			console.log("error trying to insert document into database");
-			throw error;
-		}
-
-		console.log("Le document a bien été inséré");    
-	});
-	
-	//on essaye de retrouver un doc
-//	var objToFind = {name: "coucou"};
-	collection.findOne({name: "coucou", game: "Super Mario Land"}, function(err, document) {
-		console.log("err="+err);
-		if (err){
-			console.log("error in \'findOne\'");
-			throw err;
-		}
-		if (document)
-			console.log("La date recherchée est: "+document.date);
-	});
-
-	
-	//lecture document
-	db.collection("personnages").find().toArray(function (error, results) {
-		if (error) {
-			console.log("error trying to find collection");
-			throw error;
-		}
-		
-        results.forEach(function(obj, i) {
-			console.log(i);
-			console.log(obj);
-            console.log(
-                "ID : "  + obj._id.toString() + "\n" + //comment
-                "Nom : " + obj.name + "\n" +           // Adrian Shephard
-                "Jeu : " + obj.game                  // Half-Life: Opposing Force
-            );
-        });
-		console.log("error="+error+"\n"+"results="+results);
-    });
-	
-});
-*/
-
-/************************************/
 
 /********************/
 //server.use(server.router);
 server.use(express.static('public'));
-console.log("dirname="+__dirname);
 server.use("/scripts", express.static(__dirname+'/public'));
 /********************/
 
@@ -114,23 +39,12 @@ server.use("/scripts", express.static(__dirname+'/public'));
 	
 //html display
 server.get('/', function(request, response) {
-/*
 
-	fs.unlink(__dirname+"/public/display.js", function (err) {
-		if (err) {
-			console.log("error while deleting display.js");
-			throw err;
-		}
-		console.log ( "Fichier supprimé avec succès!");
-		//response.sendFile(__dirname + '/public/index.html');
-	});		*/
-	
 	response.sendFile(__dirname + '/public/index.html');
-	
-	
 	
 });
 
+//POST request
 server.post('/', function(request, response) {
 	var serverResponse = response;
 	
@@ -156,26 +70,29 @@ server.post('/', function(request, response) {
 		
 		
 		
-		//look for request in database
-		function lookInDB() {
+		//process request
+		function processRequest() {
+			//check if known request
 			collection.findOne({from: From.toUpperCase(), to: To.toUpperCase(), textarea: Text}, function(err, document) {
 				if (err){
 					console.log("error in \'findOne\'");
 					throw err;
 				}
-				console.log("found something (maybe ?)");
 				
+				//known request
 				if(document)
 				{
-					console.log("document dans la db");
-					console.log("la traduction est : "+document.translation);
-					writeResponse(document.translation);
+					console.log("Known request");
+					console.log("result : "+document.translation);
+					writeResponse(document.from, document.to, document.textarea, document.translation);
 					//send result to client
 					serverResponse.sendFile(__dirname + '/public/index.html');
 				}
+				
+				//unknown request
 				else
 				{
-					console.log("le doc n'est pas dans la db");
+					console.log("Request not known yet. Calling Yandex API...");
 					//TODO cas error != null
 					APIrequest(yandexURL+"key="+yandexKey+"&text="+Text+"&lang="+From+'-'+To+"&[format=plain]", function (error, response, body) {
 						if(error)
@@ -184,9 +101,6 @@ server.post('/', function(request, response) {
 							throw error;
 						}
 						
-						console.log('API request: error:', error); // Print the error if one occurred 
-						console.log('API request: statusCode:', response && response.statusCode); // Print the response status code if a response was received 
-						console.log('API request: body:', body); // Print the body
 						
 						var info = JSON.parse(body);
 
@@ -198,44 +112,36 @@ server.post('/', function(request, response) {
 							//TODO
 						}
 						
-						console.log("coucou");
-						writeResponse(info.text);
+						//save answer
+						writeResponse(From.toUpperCase(), To.toUpperCase(), Text, info.text);
 						console.log("text="+info.text);
 						
-						//database write
-						console.log("jusqu'ici c'est bon");
+						//write into database
 						var objNew = { from: From.toUpperCase(), to: To.toUpperCase(), textarea: Text, translation: info.text };
-						console.log("jusqu'ici c'est bon aussi: objNew="+objNew.from+' '+objNew.to+ ' '+objNew.textarea+ ' '+objNew.translation);
+
 						collection.insert(objNew, null, function (error, results) {
 							if (error) {
 								console.log("error trying to insert document into database");
 								throw error;
 							}
 
-							console.log("Le document a bien été inséré");    
+							console.log("new document inserted into database");    
 							
-								///////////////////////////////////lecture document
 
-								///////////////////////////////////
 							//send result to client
 							serverResponse.sendFile(__dirname + '/public/index.html');
 						});
 					});
 					
-				}
-/********************************************/				
+				}			
 				
 			});
 		}
 		
 		
-		lookInDB();
+		processRequest();
 
 	});
 	
-	//setTimeout(function(){response.sendFile(__dirname + '/public/index.html')},1000); 
-	
-	//response.send(From + ' ' + To + ' ' + Text + ' ' + yandexKey);
-	//response.sendFile(__dirname + '/public/index.html');
 });
 server.listen(8080);
